429da9b45bfb1876c73fab7cc80c093d16144970
Andrzej Kretowicz <andrzej@kofeinka.net>
2019-02-28 18:35:26 +0100

    Add dist

---
+++ dist/precache-manifest.ebaa125a76587f1bbe937624ba6d8a78.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "d4360a1f7e7595e9ea0e",
+    "url": "/js/chunk-vendors.af910158.js"
+  },
+  {
+    "revision": "ab43a620ec127044bf64",
+    "url": "/js/app.ab56794a.js"
+  },
+  {
+    "revision": "7b2b37636326e37bb3c50ece3d6ac0bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f0a76321d30f3c8120915e57f7bd77e",
+    "url": "/fonts/element-icons.6f0a7632.ttf"
+  },
+  {
+    "revision": "2fad952a20fbbcfd1bf2ebb210dccf7a",
+    "url": "/fonts/element-icons.2fad952a.woff"
+  },
+  {
+    "revision": "d4360a1f7e7595e9ea0e",
+    "url": "/css/chunk-vendors.99073a56.css"
+  },
+  {
+    "revision": "ab43a620ec127044bf64",
+    "url": "/css/app.5e21057d.css"
+  }
+];
